19e4771bda47aad021181d07ca10a8c9073c2dd4
kevin__cai <caicanghao@gmail.com>
2023-04-19 17:26:14 +0800

    import inquirer

--- index.ts
+++ index.ts
@@ -1,24 +1,25 @@
-import fs from "fs";
-// import prompts from "prompts";
-import resolve from "path";
-import { getAllDrive } from "./src/utils";
-const prompts = require("prompts");
+// import fs from "fs";
+// // import prompts from "prompts";
+// import resolve from "path";
+// import { getAllDrive } from "./src/utils";
+// const inquirer = require("inquirer");
+import inquirer from "inquirer";
 
-async function readFileList(path, filesList) {
-  var files = fs.readdirSync(path);
-  files.forEach(function (itm, index) {
-    var stat = fs.statSync(path + itm);
-    if (stat.isDirectory()) {
-      //递归读取文件
-      readFileList(path + itm + "/", filesList);
-    } else {
-      var obj = {}; //定义一个对象存放文件的路径和名字
-      // obj.path = path;//路径
-      // obj.filename = itm//名字
-      filesList.push(obj);
-    }
-  });
-}
+// async function readFileList(path, filesList) {
+//   var files = fs.readdirSync(path);
+//   files.forEach(function (itm, index) {
+//     var stat = fs.statSync(path + itm);
+//     if (stat.isDirectory()) {
+//       //递归读取文件
+//       readFileList(path + itm + "/", filesList);
+//     } else {
+//       var obj = {}; //定义一个对象存放文件的路径和名字
+//       // obj.path = path;//路径
+//       // obj.filename = itm//名字
+//       filesList.push(obj);
+//     }
+//   });
+// }
 
 // getAllDrive().then((res) => {
 //   console.log(res);
@@ -36,30 +37,34 @@ async function readFileList(path, filesList) {
 
 // console.log("__dirname : " + __dirname);
 // console.log("cwd       : " + process.cwd());
-
-// prompts.inject(["@terkelg", ["#ff0000", "#0000ff"]]);
-
-(async () => {
-  const response = await prompts([
+inquirer
+  .prompt([
     {
-      type: "text",
-      name: "twitter",
-      message: `What's your twitter handle?`,
-    },
-    {
-      type: "multiselect",
-      name: "value",
-      message: "Pick colors",
+      type: "list",
+      name: "theme",
+      message: "What do you want to do?",
       choices: [
-        { title: "Red", value: "#ff0000" },
-        { title: "Green", value: "#00ff00", disabled: true },
-        { title: "Blue", value: "#0000ff", selected: true },
+        "Order a pizza",
+        "Make a reservation",
+        new inquirer.Separator(),
+        "Ask for opening hours",
+        {
+          name: "Contact support",
+          disabled: "Unavailable at this time",
+        },
+        "Talk to the receptionist",
       ],
-      max: 2,
-      hint: "- Space to select. Return to submit",
     },
-  ]);
-
-  console.log(response);
-  // => { twitter: 'terkelg', color: [ '#ff0000', '#0000ff' ] }
-})();
+    {
+      type: "list",
+      name: "size",
+      message: "What size do you need?",
+      choices: ["Jumbo", "Large", "Standard", "Medium", "Small", "Micro"],
+      filter(val) {
+        return val.toLowerCase();
+      },
+    },
+  ])
+  .then((answers) => {
+    console.log(JSON.stringify(answers, null, "  "));
+  });
